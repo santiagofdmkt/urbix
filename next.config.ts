a0b81a7667b8f3ede03a31ea5a69e6f357da4511
Santiago <santiagofdmkt@gmail.com>
2026-05-29 00:17:15 -0300

    fix: parsear imagenes JSON string

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      'zrtaffecubpwoxjycyse.supabase.co',
+      'www.argenprop.com',
+      'imgar.zonapropcdn.com',
+    ],
+  },
 };
 
 export default nextConfig;
